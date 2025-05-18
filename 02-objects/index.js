5bbbb9bbf265fa54f4070163bf70f5d0e56834fe
Zayn Raza <zaynraza.dev@gmail.com>
2025-05-19 03:50:34 +0500

    Object creation and manipulation

---
+++ 02-objects/index.js
@@ -0,0 +1,15 @@
+const user = {
+  name: "Zayn",
+  age: 25,
+  idDeveloper: true,
+  address: {
+    street: "123 Main St",
+    city: "Anytown",
+    state: "CA",
+    zip: "123456",
+  },
+};
+
+user.favFruits = ["Mango", "Banana", "Apple", "Orange", "Grapes"];
+
+console.log(user);
