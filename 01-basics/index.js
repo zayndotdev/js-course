52790e0a904b09161c3ec0ed539b2c428229eff1
Zayn Raza <zaynraza.dev@gmail.com>
2025-05-18 05:07:55 +0500

    variable scope and hoisting

--- 01-basics/index.js
+++ 01-basics/index.js
@@ -1,1 +1,13 @@
-console.log("Hello, World!!!");
+// console.log("Hello, World!!!");
+// const myName = "zayn";
+// let age = 25;
+
+// console.log(`Hello, This is ${myName} and I am ${age} years old.`);
+
+function something() {
+  console.log("Hello, World!!!");
+  var name = "zayn";
+  console.log(name);
+}
+
+something();
