b08c52326f932ffd70d3a7237ca399a21eeada92
Zayn Raza <zaynraza.dev@gmail.com>
2025-05-19 06:23:15 +0500

    object completed

--- 02-objects/index.js
+++ 02-objects/index.js
@@ -8,8 +8,40 @@ const user = {
     state: "CA",
     zip: "123456",
   },
+
+  greet: function () {
+    // console.log(this);
+    console.log(
+      `Hello, my name is ${this.name} and I am ${this.age} years old.`
+    );
+  },
 };
 
+// add properties to the user object
 user.favFruits = ["Mango", "Banana", "Apple", "Orange", "Grapes"];
 
-console.log(user);
+// logging the user object
+// console.log(user);
+
+user.greet();
+
+/*
+    creating a constrotor function
+*/
+
+function User(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+User.prototype.userDetailes = function () {
+  console.log(`Name: ${this.name}\nAge: ${this.age}`);
+};
+
+const user1 = new User("Ali", 22);
+const user2 = new User("Zayn", 25);
+
+console.log(user1);
+console.log(user2);
+user1.userDetailes();
+user2.userDetailes();
